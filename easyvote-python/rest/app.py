a5d364b69d238dd634ab15c4a3c93a384227ac67
Himanshu Aggarwal <aggarwal90himanshu@gmail.com>
2018-09-16 21:21:32 +0530

    Used flask CROS

--- easyvote-python/rest/app.py
+++ easyvote-python/rest/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from user import User
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route("/")
 # def hello():
