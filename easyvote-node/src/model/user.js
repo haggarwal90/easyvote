f84cd70c39eb4f92c6f79405237e138cbe64a9dd
Himanshu Aggarwal <aggarwal90himanshu@gmail.com>
2018-11-09 00:18:39 +0530

    Added docker fiel and updates package dependencies

--- easyvote-node/src/model/user.js
+++ easyvote-node/src/model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -21,9 +21,9 @@ UserSchema.pre('save', async function(next){
   const user = this;
   //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
   //your application becomes.
-  const hash = await bcrypt.hash(this.password, 10);
+  // const hash = await bcrypt.hash(this.password, 10);
   //Replace the plain text password with the hash and then store it
-  this.password = hash;
+  this.password = this.password;
   //Indicates we're done and moves on to the next middleware
   next();
 });
@@ -33,8 +33,8 @@ UserSchema.methods.isValidPassword = async function(password){
   const user = this;
   //Hashes the password sent by the user for login and checks if the hashed password stored in the 
   //database matches the one sent. Returns true if it does else false.
-  const compare = await bcrypt.compare(password, user.password);
-  return compare;
+  // const compare = await bcrypt.compare(password, user.password);
+  return password === user.password;
 }
 
 const UserModel = mongoose.model('user',UserSchema);
